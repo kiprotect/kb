9a6998918b811a4708808de6de7c12758461638d
Andreas Dewes <andreas.dewes@7scientists.com>
2018-04-29 12:38:24 +0200

    Make sure footer is always at the bottom of the page.

--- themes/docs/static/js/bulma.js
+++ themes/docs/static/js/bulma.js
@@ -1,5 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+    //we adapt the padding of the body element to the actual footer height
+    var body = document.getElementsByTagName('body')[0]
+    var footer = document.getElementById('footer')
+    body.style.paddingBottom = footer.offsetHeight+'px'
+
     var toggles = document.querySelectorAll('[data-toggle]').forEach(
         function(toggle){
             var target = toggle.dataset.target
